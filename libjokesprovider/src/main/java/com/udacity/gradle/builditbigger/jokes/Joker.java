e78f8f2717dcc0345bb659e7fda0f93195a38007
Prateesh Goswami <prateeshgoswami@gmail.com>
2016-05-23 14:55:11 -0700

    Feat: Add Java library to provide jokes

---
+++ libjokesprovider/src/main/java/com/udacity/gradle/builditbigger/jokes/Joker.java
@@ -0,0 +1,7 @@
+package com.udacity.gradle.builditbigger.jokes;
+
+public class Joker {
+    public String getJoke(){
+                return "Knock Knock;who's there; Boo; boo who;Don't cry I just want to com in";
+            }
+}
